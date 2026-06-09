65fdf908aa26c1938cc005d3670ad8ed0a32075c
harix11 <harixnarayanan11@gmail.com>
2026-06-09 21:09:25 +0530

    Add full adder module in Verilog
    Initial implementation of a full adder module in Verilog.

--- day1/Ripple_Carry_Adder/design/full_adder.v
+++ day1/Ripple_Carry_Adder/design/full_adder.v
@@ -1,1 +1,32 @@
 
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.06.2026 11:57:19
+// Design Name: 
+// Module Name: full_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module full_adder(input a,b,c,output s,cout);
+wire w1,w2,w3,w4;
+xor(w1,a,b);
+xor(s,w1,c);
+and(w2,b,c);
+and(w3,b,a);
+and(w4,a,c);
+or(cout,w2,w3,w4);
+endmodule
