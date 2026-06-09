54ee5ef88fd5addb2618dceda451dd9c32d7259c
harix11 <harixnarayanan11@gmail.com>
2026-06-09 21:20:45 +0530

    usr_shift register
    Removed unnecessary comments to streamline the code.

--- Day2/USR/Design/usr.v
+++ Day2/USR/Design/usr.v
@@ -1,23 +1,6 @@
 
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.06.2026 20:30:02
-// Design Name: 
-// Module Name: usr_shiftreg
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+
 //////////////////////////////////////////////////////////////////////////////////
 
 
