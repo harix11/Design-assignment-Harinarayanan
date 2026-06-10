2b82fd7574d30c253bfbb1cee29873b9e3af318f
harix11 <harixnarayanan11@gmail.com>
2026-06-10 21:12:21 +0530

    Update machine1.v

--- Day 3/Fifo/Design/machine1.v
+++ Day 3/Fifo/Design/machine1.v
@@ -1,1 +1,17 @@
 
+module machine1(
+    input wire clk,
+    input wire rst,
+    output reg [7:0] data_out,
+    output reg wr_en
+);
+    always @(posedge clk) begin
+        if (rst) begin
+            data_out <= 8'h00;
+            wr_en    <= 1'b0;
+        end else begin
+            data_out <= data_out + 1'b1;
+            wr_en    <= 1'b1;
+        end
+    end
+endmodule
